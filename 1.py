bc827bfc21ef2cd8218b44c9c2889c534c18bded
BellaLu2009 <bellalu20091208@gmail.com>
2026-03-08 16:45:05 +0800

    罗马数字

---
+++ 1.py
@@ -0,0 +1,26 @@
+# 关键字练习
+if True:
+    print(True)
+
+if False:
+    print(False)
+
+else:
+    print("False没有被打印")
+
+for i in range(10):
+    print(i)
+
+# class A:
+#     def __init__(self):
+def abc(a,b):
+    return a + b
+    print(a+b)
+print(abc(1,2))
+print("aaaaaaa")
+a=0
+b=0
+c=1
+while a+b==c:
+    print(a+b)
+
--- 罗马数字转整数.py
+++ 罗马数字转整数.py
@@ -1,6 +1,37 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        return 0
+    def romanToInt(self, s):
+        romannumber={"I":1, "V": 5,"X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,}
+        if s in romannumber:
+            print(romannumber[s])
+        ans = 0
+        for i in range(len(s)-1):
+            print(i, s[i])
+            key=s[i]
+            romannumber[key]
+            if romannumber[s[i]] < romannumber[s[i + 1]]:
+                ans -= romannumber[key]
+            else:
+                ans += romannumber[key]
+        key = s[-1]
+        ans += romannumber[key]
+        print(ans)
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     '''罗马数字包含以下七种字符: I， V， X， L， C， D 和 M。
@@ -16,15 +47,17 @@ M             1000
 
 例如， 罗马数字 2 写做 II ，即为两个并列的 1 。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
 
-通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数
+等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
 
 I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
 X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。
 C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
 给定一个罗马数字，将其转换成整数。'''
     solution = Solution()
-    s = "III"
+    s = "LXXIX"
     result = solution.romanToInt(s)
     print(result)
     # 期望的输出结果是 3
-    assert result == 3
+    # assert result == 3
+
