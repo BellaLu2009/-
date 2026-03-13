f6a2506a78de0e49425aba95104cca93bfd4bd73
BellaLu2009 <bellalu20091208@gmail.com>
2026-03-13 19:43:03 +0800

    修改

--- 删除有序数组中的重复项.py
+++ 删除有序数组中的重复项.py
@@ -2,15 +2,20 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # 存放去重之后的数据
         only_nums = []
+        # 用来去重
         dic_1={}
+        # 将原列表数据存入字典
         for a in nums:
             dic_1[a]=0
         print(dic_1)
+        # 把字典里的键存入新列表
         for a in dic_1:
             only_nums.append(a)
-            return only_nums
-    print(len(only_nums)）
+        print(len(only_nums), only_nums)
+        return len(only_nums),only_nums
+
 
 if __name__ == '__main__':
     '''给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的相对顺序应该保持 一致 。然后返回 nums 中唯一元素的个数。
@@ -31,8 +36,12 @@ if __name__ == '__main__':
 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。'''
     solution = Solution()
     nums = [1,1,2]
-    k = solution.removeDuplicates(nums)
+    k,nums = solution.removeDuplicates(nums)
     print(k, nums)
     # 期望的输出结果是 2, [1, 2]
     assert k == 2
     assert nums[:k] == [1, 2]
+
+
+
+#     有序字典
